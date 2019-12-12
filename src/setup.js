c908d1bc33bcb19572d53ad325f3ee8480f01317
Tamal Patra <patra.tamal@gmail.com>
2019-12-12 20:38:43 +0530

    ga update

--- src/setup.js
+++ src/setup.js
@@ -1,12 +1,19 @@
 const replace = require('replace-in-file');
 
 let gaEnabled = 'true' === (process.env.GA_ENABLED || false).toString(),
-  gaTrackingId = (process.env.GA_TRACKING_ID || 'XXXX').toString();
+  gaTrackingId = (process.env.GA_TRACKING_ID || 'XXXX').toString(),
+  gaContext = (process.env.CONTEXT || 'XXXX').toString(),
+  gaGitCommit = (process.env.COMMIT_REF || 'XXXX').toString();
 
 const replacement = {
   files: 'themes/cactus/_config.yml',
-  from: ["'{{GA_ENABLED}}'", "'{{GA_TRACKING_ID}}'"],
-  to: [gaEnabled.toString(), gaTrackingId]
+  from: [
+    "'{{GA_ENABLED}}'",
+    "'{{GA_TRACKING_ID}}'",
+    "'{GA_CONTEXT}'",
+    "'{GA_GIT_COMMIT}'"
+  ],
+  to: [gaEnabled.toString(), gaTrackingId, gaContext, gaGitCommit]
 };
 
 console.log(replace.sync(replacement));
