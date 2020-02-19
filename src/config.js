e265bf536960e5cf618c594c21d44006c84f4c04
Tamal Patra <patra.tamal@gmail.com>
2020-02-20 00:25:13 +0530

    Reality, 2020

--- src/config.js
+++ src/config.js
@@ -10,5 +10,6 @@ module.exports = {
   gaEnabled: 'true' === (process.env.GA_ENABLED || false).toString(),
   gaTrackingId: (process.env.GA_TRACKING_ID || 'XXXX').toString(),
   gaContext: (process.env.CONTEXT || 'XXXX').toString(),
-  gaGitCommit: (process.env.COMMIT_REF || 'XXXX').toString()
+  gaGitCommit: (process.env.COMMIT_REF || 'XXXX').toString(),
+  envs: envList.parsed
 };
