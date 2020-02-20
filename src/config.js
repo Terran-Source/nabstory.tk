4876d96e4df025b60a988a5ba138a732743e051c
Tamal Patra <patra.tamal@gmail.com>
2020-02-20 23:42:58 +0530

    using @terran-source/dotconfig instead of dotenv

--- src/config.js
+++ src/config.js
@@ -1,15 +1,9 @@
-const dotenv = require('dotenv');
+const { loadConfig } = require('@terran-source/dotconfig');
 
-let envList = dotenv.config();
+let envList = loadConfig('config.json');
 
 if (envList.error) throw envList.error;
 
 // console.log(envList.parsed);
 
-module.exports = {
-  gaEnabled: 'true' === (process.env.GA_ENABLED || false).toString(),
-  gaTrackingId: (process.env.GA_TRACKING_ID || 'XXXX').toString(),
-  gaContext: (process.env.CONTEXT || 'XXXX').toString(),
-  gaGitCommit: (process.env.COMMIT_REF || 'XXXX').toString(),
-  envs: envList.parsed
-};
+module.exports = envList.parsed;
